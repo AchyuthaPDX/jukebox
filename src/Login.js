0b451e9d8541208b600efb1a4d2f3dba5dea33de
Achyutha Harish <91561702+AchyuthaPDX@users.noreply.github.com>
2021-11-26 16:17:23 -0800

    Added Dashboard and feature tested

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Container} from 'react-bootstrap'
 
-let auth = "https://accounts.spotify.com/authorize?client_id=b084c467a92e45688e0f88d1f96f4316&response_type=code&redirect_uri=http://localhost:3000&scope=stream%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-statw%20user-modify-playback-state"
+let auth = "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function login(){
     return( <Container
-        classname="d-flex justify-content-center align-items-center"
-        style={{minHeight:"100vh"}}>
-        <a class="btn btn-success btn-lg" href={auth}>Welcome to Savaan</a>
+        className="d-flex justify-content-center align-items-center bgcolor-blue"
+        style={{minHeight:"100vh"
+    /*backgroundImage: ("https://iotgadgets.com/wp-content/uploads/2017/01/Smartphone-App-Saavn-Music-Samsung-Z1-Z2-Z3-Tizen-Experts-Store-700.png")*/}}>
+        <a class="btn btn-success btn-lg" href={auth}>Savaan</a>
     </Container>
     )
 }
