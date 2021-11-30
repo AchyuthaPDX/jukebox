88986028bfa83c2c7b9b70b79088d40f05387d35
Achyutha Harish <91561702+AchyuthaPDX@users.noreply.github.com>
2021-11-29 16:47:40 -0800

    Added Track Player File

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react"
 import useAuth from "./useAuth"
-//import TrackSearchResult from "./TrackSearchResult"
+import TrackSearchResult from "./TrackSearchResult"
+import Player from"./Player"
 import { Container , Form } from "react-bootstrap"
 import SpotifyWebApi from "spotify-web-api-node"
 
@@ -11,7 +12,13 @@ export default function Dashboard({code}){
     const accessToken = useAuth(code)
     const [search , setSearch] = useState("")
     const [searchResults, setSearchResults] = useState([])
-    console.log(searchResults)
+    const [playingTrack, setPlayingTrack] = useState()
+
+    function chooseTrack(track){
+        setPlayingTrack(track)
+        setSearch('')
+    }
+
     useEffect(()=> {
         if (!accessToken) return
         spotifyApi.setAccessToken(accessToken)
@@ -19,7 +26,9 @@ export default function Dashboard({code}){
     useEffect(() => {
         if (!search) return setSearchResults([])
         if (!accessToken) return
+        let cancel = false
         spotifyApi.searchTracks(search).then(res =>{
+            if(cancel) return
             setSearchResults(res.body.tracks.items.map(track => {
                 const smallestAlbumImage = track.album.images.reduce(
                     (smallest,image)=> {
@@ -36,6 +45,7 @@ export default function Dashboard({code}){
                 }
             }))
         })
+        return () => cancel = true
     } , [search, accessToken])  
     
     
@@ -45,86 +55,10 @@ export default function Dashboard({code}){
           onChange={e => setSearch(e.target.value)}
         />
         <div className="flex-grow-1 my-2" style ={{overflowY:"auto"}}>
-            songs
-        </div>
-    </Container>
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import {useState, useEffect} from "react"
-import useAuth from "./useAuth"
-import TrackSearchResult from "./TrackSearchResult"
-import { Container , Form } from "react-bootstrap"
-import SpotifyWebApi from "spotify-web-api-node"
-
-const spotifyApi = new SpotifyWebApi({
-    client_id:"b945ef10ea24755b83ac50cede405a0"
-})
-
-export default function Dashboard({ code }){
-    const accessToken = useAuth(code)
-    const [search , setSearch] = useState('')
-    const [searchResults, setSearchResults] = useState([])
-    useEffect(() => {
-        if (!accessToken) return
-        spotifyApi.setAccessToken(accessToken)
-      }, [accessToken])
-      useEffect(() => {
-        if (!search) return setSearchResults([])
-        if (!accessToken) return
-        let cancel = false
-        spotifyApi.searchTracks(search).then(res => {
-          if (cancel) return
-          setSearchResults(
-            res.body.tracks.items.map(track => {
-              const smallestAlbumImage = track.album.images.reduce(
-                (smallest, image) => {
-                  if (image.height < smallest.height) return image
-                  return smallest
-                },
-                track.album.images[0]
-              )
-    
-              return {
-                artist: track.artists[0].name,
-                title: track.name,
-                uri: track.uri,
-                albumUrl: smallestAlbumImage.url,
-              }
-            })
-          )
-        })
-    
-        return () => (cancel = true)
-      }, [search, accessToken])
-
-
-    return (
-        <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
-        <Form.Control
-          type="search"
-          placeholder="Search Songs/Artists"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-        <div className="flex-grow-1 my-2" style ={{overflowY:"auto"}}>
-            {searchResults.map(track =>(
-                <TrackSearchResult track ={track} key={track.uri} />
+        {searchResults.map(track =>(
+                <TrackSearchResult track ={track} key={track.uri} chooseTrack = {chooseTrack} />
             ))}
         </div>
-        </Container>
-    )
-}*/
+        <div> <Player accessToken={accessToken} trackUri={playingTrack?.uri} /></div>
+    </Container>
+}
--- src/TrackSearchResult.js
+++ src/TrackSearchResult.js
@@ -1,4 +1,24 @@
+import React from "react"
+ 
+export default function TrackSearchResult({track, chooseTrack}) {
+  function handlePlay() {
+    chooseTrack(track)
+  }
+  return(
+    <div className="d-flex m-2 align-items-center" style={{cursor: "pointer"}} onClick={handlePlay}>
+      <img src={track.albumUrl} style = {{ height:"64px" , width:"64px" }} />
+      <div className="ml-4">
+        <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
+      </div>
+      
+    </div>
+  )
+}
+ 
+ 
  /*import React from "react"
+
 
 export default function TrackSearchResult({ track, chooseTrack }) {
   function handlePlay() {
